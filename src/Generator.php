f51df0e4bb3fc4b441e828ad0ba44005991c8ab8
Kazimierz Matan <kazimierz.matan@gmail.com>
2020-10-07 18:19:35 +0200

    Generator class created

---
+++ src/Generator.php
@@ -0,0 +1,20 @@
+<?php
+namespace Temere;
+
+class Generator {
+
+    private function __construct() {
+        $types = ["a", "A", "1", "-"];
+        $methods = ['brute', 'hash', 'uniqid', 'randombytes'];
+    }
+
+    public function generate($type = "aA1-", $length = 20) {
+
+            $method = $this->selectMethod();
+            return $this->methods[$method];
+    }
+
+    private function selectMethod() {
+        return mt_rand(0, count($this->methods)-1);
+    }
+}
